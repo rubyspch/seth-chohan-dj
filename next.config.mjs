b6f072d737b713e1e396a72399139b8bec9c4243
Ruby Chohan <ruby.chohan@cancer.org.uk>
2024-11-18 17:23:37 +0000

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/seth-chohan-dj",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
